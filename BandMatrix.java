cf0358941c9887981ccbeeb90a5f120521328216
vladimirgligic <58168049+vladimirgligic@users.noreply.github.com>
2021-10-28 12:22:01 +0200

    Update BandMatrix.java

--- BandMatrix.java
+++ BandMatrix.java
@@ -1,7 +1,5 @@
 /*
-Band matrices. 
-Write a program BandMatrix.java 
-that takes two integer command-line arguments n and width and prints an n-by-n pattern, 
+Takes two integer command-line arguments n and width and prints an n-by-n pattern, 
 with a zero (0) for each element whose distance from the main diagonal is strictly more than width, 
 and an asterisk (*) for each entry that is not, and two spaces between each 0 or *.
  */
